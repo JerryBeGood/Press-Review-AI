36f015e57e15cf75cb453777057d2311ec6a5619
Szybujący Orzeł <mdobrowolski.1997@gmail.com>
2025-09-17 19:31:17 +0200

    Delete stale comment

--- models.js
+++ models.js
@@ -1,8 +1,7 @@
 import { anthropic } from '@ai-sdk/anthropic';
 import { openai } from '@ai-sdk/openai';
 
-// TODO: There must be a different model then OpenAI because of the privacy concerns -> DeepSeek R1, Llama4 or Claude-Sonnet-3.7
-// 
+
 const models = {
   main: openai('o4-mini'),
   primary: anthropic('claude-sonnet-4-20250514'),

a8508c20eea5c8269703268f1cbc8cd053b7e673
Szybujący Orzeł <mdobrowolski.1997@gmail.com>
2025-10-03 13:54:22 +0200

    fix: use import.meta.enc instead of process.env in utils.ts

--- src/util.ts
+++ src/util.ts
@@ -2,7 +2,7 @@ function validateSecrets(): void {
     const secrets: string[] = ['OPENAI_API_KEY', 'EXASEARCH_API_KEY', 'ANTHROPIC_API_KEY'];
   
     for(const secret of secrets) {
-      if (!process.env[secret]) {
+      if (!import.meta.env[secret]) {
         console.error(`Error: Missing ${secret}. Please set it in your environment or .env file.`);
   
         process.exit(1);

f9ba8ec208cbb0717543ee21a2efdca4dd06d8d0
Szybujący Orzeł <mdobrowolski.1997@gmail.com>
2025-11-15 13:44:59 +0100

    chore(config): update Playwright configuration to use .env.test and change test directory structure

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig, devices } from "@playwright/test";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(process.cwd(), ".env.test") });
 
 // https://playwright.dev/docs/test-configuration
 export default defineConfig({
-  testDir: "./tests-e2e",
+  testDir: "./tests/e2e",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
